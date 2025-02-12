4441faff124b73fee79978726fc3031511364ee5
Akashpixel Digital <dev@akashpixel.com>
2025-02-12 12:21:38 +0530

    Update TaskCommentThread.tsx - file name 25

--- src/components/project-layouts/maintenance/comments/TaskCommentThread.tsx
+++ src/components/project-layouts/maintenance/comments/TaskCommentThread.tsx
@@ -92,7 +92,7 @@ const getFileName = (url: string) => {
   const fileName = decodeURIComponent(url.split('/').pop() || '');
   const nameWithoutExt = fileName.slice(0, fileName.lastIndexOf('.')) || fileName; // Handle files with no extension
 
-  return nameWithoutExt.length <= 15 ? nameWithoutExt : `${nameWithoutExt.slice(0, 15)}...`;
+  return nameWithoutExt.length <= 25 ? nameWithoutExt : `${nameWithoutExt.slice(0, 25)}...`;
 };
 
 
