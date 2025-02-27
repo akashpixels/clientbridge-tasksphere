ddcf29146426349fc324cda64d3d475f233279d5
Akashpixel Digital <dev@akashpixel.com>
2025-02-28 00:25:10 +0530

    Update Clients.tsx- client admin removal

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -68,9 +68,7 @@ const Clients = () => {
   return <div className="container mx-auto p-6">
       <h1 className="text-2xl font-semibold mb-6">Clients</h1>
       <Card>
-        <CardHeader>
-          <CardTitle>Client Admins</CardTitle>
-        </CardHeader>
+   
         <CardContent>
           {isLoading ? <p className="text-center py-4">Loading client administrators...</p> : <div className="rounded-md border">
               <Table>
